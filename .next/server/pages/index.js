2ee11b209645dc8d54f3b60c8a8f448bafa80f16
abraham <abraham.turnsek@gmail.com>
2023-05-25 15:38:44 +0200

    version 1.0

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -1672,7 +1672,7 @@ const Section_uno = ()=>{
                 children: [
                     /*#__PURE__*/ (0,jsx_runtime_.jsxs)("h1", {
                         children: [
-                            "Compro oro en",
+                            "Compro oro ",
                             /*#__PURE__*/ jsx_runtime_.jsx("span", {
                                 className: (section_uno_module_default()).linea,
                                 children: "Tarragona"
